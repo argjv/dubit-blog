45a7a2da4b384a64c1a7465ac4200c74e7f5557c
Jorge Valdeiglesias <jorge@bitgo.com>
2018-08-19 11:41:29 -0700

    Add initial invoice logic

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,22 @@ var bitreader = require('bitreader');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  bitreader.getInfo('34.208.184.134', 10009, function (response) {
-    res.render('index', { title: 'Express',  getInfo: response});
+  var ip = '34.208.184.134';
+  var port = 10009;
+  var amount = 100400;  // satoshies
+  var articleTitle = 'Astronaut Capital invests in Lendingblock (LND) Private Sale';
+  bitreader.getInfo(ip, port, function (response) {
+    bitreader.subscribeToInvoices(ip, port, function (invoice) {
+      console.log('Got new invoice' + invoice);
+      // Remove lnd_payment and lnd_info divs
+      // Enable scrolling or article visibility
+      // Refresh the page
+    });
+    bitreader.generateInvoice(ip, port, amount, 'Paying for ' + articleTitle, function (invoice) {
+      invoice.value = amount;
+      invoice.memo = 'Paying for ' + articleTitle;
+      res.render('index', { title: articleTitle,  getInfo: response, invoice: invoice});
+    });
   });
 });
 
